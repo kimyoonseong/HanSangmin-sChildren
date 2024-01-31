ca39c9df0fd93adc39b70d777ff94e2a4b2955e0
'bseongwook' <'qbdmlghkfdyd@gmail.com'>
2024-01-31 17:20:15 +0900

    CREATE BoardService.java

--- foodRec/src/main/java/com/example/demo/service/BoardService.java
+++ foodRec/src/main/java/com/example/demo/service/BoardService.java
@@ -33,30 +33,30 @@ public class BoardService {
 	}
 	
 	// 전체읽기 - 페이지로 끊어서 값을 가지고 오는 방법 존재 - 221p
-		public Page<Board> listBoard(int page){
-			// 최신순 정렬 - 내림차순
-			Pageable pageable = PageRequest.of(page, 5, Direction.DESC, "no");
-			Page<Board> pageInfo = brepo.findAll(pageable);
-			return pageInfo;
-		}
+	public Page<Board> listBoard(int page){
+		// 최신순 정렬 - 내림차순
+		Pageable pageable = PageRequest.of(page, 5, Direction.DESC, "no");
+		Page<Board> pageInfo = brepo.findAll(pageable);
+		return pageInfo;
+	}
 
-		// 하나읽기
-		public Board detailBoard(int no) {
-			// 값이 없는 경우 종료되는게 아니라 에러를 제시하기 위해
-			Optional<Board> option = brepo.findById(no); // 값의 존재 확인 가능
-			if (option.isPresent()) {
-				return option.get();
-			} else {
-				throw new RuntimeException(no + "라는 글은 없습니다.");
-			}
+	// 하나읽기
+	public Board detailBoard(int no) {
+		// 값이 없는 경우 종료되는게 아니라 에러를 제시하기 위해
+		Optional<Board> option = brepo.findById(no); // 값의 존재 확인 가능
+		if (option.isPresent()) {
+			return option.get();
+		} else {
+			throw new RuntimeException(no + "라는 글은 없습니다.");
 		}
+	}
 		
 		// 수정 - put 사용
 		
 		
 		// 삭제
-		public void deleteBoard(int no) {
-			brepo.deleteById(no);		
-		}
+	public void deleteBoard(int no) {
+		brepo.deleteById(no);		
+	}
 
 }
