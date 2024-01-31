3b7228a70d51c0c788ee3eeb2f3dc8a87a6affeb
김윤성 <dannyk97@naver.com>
2024-01-31 16:14:34 +0900

    application sql호스트 변경 (fisa)

--- foodRec/src/main/java/com/example/demo/controller/BoardController.java
+++ foodRec/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.model.dto.BoardDto;
+import com.example.demo.service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	private BoardService service;
+	
+	//객체 생성자
+	public BoardController(BoardService service) {
+		this.service=service;
+	}
+	// 레시피를 form으로 받아서 전달
+	@PostMapping("/regist") 
+	public String registBoard(@ModelAttribute BoardDto board) { 
+		//service.writeBoard(board);
+		return "redirect:/board/list";
+	}
 }
--- foodRec/src/main/java/com/example/demo/repository/BoardRepository.java
+++ foodRec/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.entity.Board;
-
+//
 public interface BoardRepository  extends JpaRepository<Board,Integer>{
 
 }
